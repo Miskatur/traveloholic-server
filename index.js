6df507dc8daf47169110b1031337b33a06163c2b
Miskatur <Miskaturrahman34826@gmail.com>
2022-11-08 17:55:23 +0600

    comments post and get method added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
-const JWT = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000;
 
 require('dotenv').config()
@@ -18,6 +18,13 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const playlistCollection = client.db('traveloholic').collection('playlist');
+        const commentCollection = client.db('traveloholic').collection('comments')
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            res.send({ token })
+        })
 
         app.get('/playlist', async (req, res) => {
             const query = {};
@@ -30,6 +37,26 @@ async function run() {
             const cursor = playlistCollection.find(query);
             const playlist = await cursor.toArray();
             res.send(playlist)
+        })
+
+        app.post('/comments', async (req, res) => {
+            const comments = req.body;
+            const result = await commentCollection.insertOne(comments)
+            res.send(result)
+        })
+
+        app.get('/comments', async (req, res) => {
+            const query = {};
+            const cursor = commentCollection.find(query);
+            const comments = await cursor.toArray();
+            res.send(comments)
+        })
+
+        app.get('/playlists/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const content = await playlistCollection.findOne(query)
+            res.send(content)
         })
 
 
